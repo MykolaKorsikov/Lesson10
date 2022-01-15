2d1b3a79e2112c9b31dd9f8c37a89d3a7466d65a
Mykola Korsikov <korsikov@ukr.net>
2022-01-15 10:15:17 +0100

    Module2

---
+++ Main.mjs
@@ -0,0 +1,10 @@
+'use strict'
+
+
+
+import { Heart as HR, Cone } from './HomeWork_Lesson10.mjs'
+import demo from './HomeWork_Lesson10.mjs'
+
+demo()
+console.log(new HR(), new Cone())
+process.exit()
